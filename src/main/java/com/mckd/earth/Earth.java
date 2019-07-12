278d1691c53f340a5a8f20b9db21d49e5470e5cd
Yuki-Shibanuma <smatosan@gmail.com>
2019-07-13 06:59:40 +0900

    create spigot

---
+++ src/main/java/com/mckd/earth/Earth.java
@@ -0,0 +1,17 @@
+package com.mckd.earth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Earth extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
