df32462452375c37ec75c76ed9bde71536aa692e
BuildTools <unconfigured@null.spigotmc.org>
2019-10-19 14:30:03 +0900

    temma

--- src/main/java/com/mckd/earth/Worlds/PveWorld.java
+++ src/main/java/com/mckd/earth/Worlds/PveWorld.java
@@ -1,16 +1,49 @@
 package com.mckd.earth.Worlds;
 
 import com.mckd.earth.Earth;
+import com.mckd.earth.Scheduler.PveScheduler;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.scoreboard.*;
 
 public class PveWorld implements Listener {
 
-    public PveWorld(Earth plugin) {
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    private  Earth plugin;
+    private  int waveCount = 1;
+    private  int enemyCount = 0;
+    public  PveWorld(Earth plugin) {
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this,plugin);
     }
 
+    @EventHandler
+    public  void enterWorld (PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        if (player.getWorld().getName().equals("pve")) {
+            player.sendMessage("Test world へようこそ");
+            player.setGameMode(GameMode.ADVENTURE);
+            player.setFoodLevel(20);
+            player.setHealth(20.0);
+            player.getWorld().setPVP(false);
+            player.getInventory().clear();
+            new PveScheduler(this.plugin,player.getWorld(),this.waveCount).runTaskTimer(this.plugin, 0, 20);
+
+            //Score board
+            ScoreboardManager sbm = Bukkit.getScoreboardManager();
+            Scoreboard sb = sbm.getMainScoreboard();
+            Objective obj = sb.getObjective("point");
+            if( obj==null) {
+                obj = sb.registerNewObjective("point", "test");
+                obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+                obj.setDisplayName("PVE");
+            }
+            Score score = obj.getScore(player.getDisplayName());
+            score.setScore(0);
+            player.setScoreboard(sb);
+        }
+    }
 }
