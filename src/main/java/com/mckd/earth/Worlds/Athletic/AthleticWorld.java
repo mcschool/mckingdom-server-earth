9b66ede880034b2deaeb0da40da8d9c90762ad26
BuildTools <unconfigured@null.spigotmc.org>
2019-11-06 16:50:01 +0900

    “アスレ追加”

--- src/main/java/com/mckd/earth/Worlds/Athletic/AthleticWorld.java
+++ src/main/java/com/mckd/earth/Worlds/Athletic/AthleticWorld.java
@@ -111,6 +111,7 @@ public class AthleticWorld implements Listener {
         this.inv.setItem(13,setItemStack(Material.EMPTY_MAP,"難しい500Mアスレ Create by taiyaki23"));
         this.inv.setItem(14,setItemStack(Material.EMPTY_MAP,"上級アスレ Create by Jikkyoreeto"));
         this.inv.setItem(15,setItemStack(Material.EMPTY_MAP,"泳げたい焼きくんアスレ Create by taiyaki23"));
+        this.inv.setItem(16,setItemStack(Material.EMPTY_MAP,"初心者アスレ　Create by momizi_tubaki"));
     }
 
     public ItemStack setItemStack(Material material, String name) {
@@ -247,6 +248,12 @@ public class AthleticWorld implements Listener {
                         Location location = new Location(player.getWorld(),-28,74,306);
                         player.teleport(location);
                         this.setConfigration(15,"泳げたい焼きくんアスレ",player,location);
+                    }
+                    //初心者アスレ
+                    if (index == 16) {
+                        Location location = new Location(player.getWorld(), 94, 70, 48);
+                        player.teleport(location);
+                        this.setConfigration(16, "初心者アスレ", player, location);
                     }
                 }
             }
