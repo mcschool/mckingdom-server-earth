a195309229eb192c8ee22910c62ca09d18657641
Yuki-Shibanuma <smatosan@gmail.com>
2019-11-25 18:04:42 +0900

    s

--- src/main/java/com/mckd/earth/Worlds/Party/SnowSpawn.java
+++ src/main/java/com/mckd/earth/Worlds/Party/SnowSpawn.java
@@ -3,7 +3,10 @@ package com.mckd.earth.Worlds.Party;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.List;
 
 public class SnowSpawn extends BukkitRunnable {
 
@@ -14,6 +17,12 @@ public class SnowSpawn extends BukkitRunnable {
         // 2025 22 -308 / 2051 23 -277
         this.count--;
         World world = Bukkit.getWorld("party");
+
+        List<Player> players = world.getPlayers();
+        for (Player p: players) {
+            p.sendTitle("RUN", "", 0, 20, 0);
+        }
+
         for (int x = 2025; x < 2051; x++) {
             for (int z = -308; z < -277; z++) {
                 world.getBlockAt(x, 24, z).setType(Material.SNOW_BALL);
