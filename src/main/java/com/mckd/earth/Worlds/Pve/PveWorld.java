e0af93e25d1381d74cc2e54c98f27ff78e1cb1ae
BuildTools <unconfigured@null.spigotmc.org>
2019-11-30 16:08:14 +0900

    “フェンス消す”

--- src/main/java/com/mckd/earth/Worlds/Pve/PveWorld.java
+++ src/main/java/com/mckd/earth/Worlds/Pve/PveWorld.java
@@ -327,18 +327,18 @@ public class PveWorld implements Listener {
                     Double  locationX2 =  Math.floor(location.getX());
                     Double  locationY2 =  Math.floor(location.getY());
                     Double  locationZ2 =  Math.floor(location.getZ());
-                    if (locationX == -497){
+                    if (locationX == -498){
                         if (locationY == 27) {
-                            if (locationZ == -116){
+                            if (locationZ == -117){
                                 location.getWorld().getBlockAt(location).setType(Material.AIR);
                                 Location location2 =new Location(location.getWorld(),++locationX2,locationY2,locationZ2);
                                 location2.getWorld().getBlockAt(location2).setType(Material.AIR);
                             }
                         }
                     }
-                    if (locationX == -497){
+                    if (locationX == -498){
                         if (locationY == 27) {
-                            if (locationZ == -117){
+                            if (locationZ == -118){
                                 location.getWorld().getBlockAt(location).setType(Material.AIR);
                                 Location location2 =new Location(location.getWorld(),--locationX2,locationY2,locationZ2);
                                 location2.getWorld().getBlockAt(location2).setType(Material.AIR);
