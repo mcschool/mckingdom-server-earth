2ad5dccd7a8bf370c403b5a9cd6f2580e112a889
BuildTools <unconfigured@null.spigotmc.org>
2019-12-04 15:23:04 +0900

    コマンド

--- src/main/java/com/mckd/earth/Commands/PveSecvetCommand.java
+++ src/main/java/com/mckd/earth/Commands/PveSecvetCommand.java
@@ -1,15 +1,29 @@
 package com.mckd.earth.Commands;
 
+import com.sun.org.apache.xml.internal.utils.ObjectVector;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 public class PveSecvetCommand {
     public  static boolean command(CommandSender sender, Command command, String label, String args[]){
         Player player = null;
         if (sender instanceof Player) {
             player = (Player) sender;
-            player.sendMessage("test");
+            ScoreboardManager sbm = Bukkit.getScoreboardManager();
+            Scoreboard sb = sbm.getMainScoreboard();
+            Objective obj = ((Scoreboard) sb).getObjective("point");
+            if( obj !=null){
+                Score score =obj.getScore(player.getDisplayName());
+                int point = (int)score.getScore();
+                score.setScore(point+2000);
+                player.setScoreboard(sb);
+            }
         }
         return  true;
     }
