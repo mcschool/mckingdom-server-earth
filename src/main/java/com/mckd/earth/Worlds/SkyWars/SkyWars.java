40e5702526af8e8dd830f7a87c3ab3c49fd098b1
BuildTools <unconfigured@null.spigotmc.org>
2019-12-14 14:21:30 +0900

    SkyWars　チェスト

--- src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
+++ src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
@@ -1,11 +1,17 @@
 package com.mckd.earth.Worlds.SkyWars;
 
 import com.mckd.earth.Earth;
+import com.mckd.earth.Worlds.Pve.PveScheduler;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+
+import java.util.List;
 
 public class SkyWars implements Listener {
 
@@ -26,6 +32,25 @@ public class SkyWars implements Listener {
                 player.getInventory().clear();
                 player.setGameMode(GameMode.SPECTATOR);
                 player.hidePlayer(this.plugin, player);
+            }
+        }
+    }
+    @EventHandler
+    public void enterWorld(PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        if (player.getWorld().getName().equals("SkyWars")) {
+            player.sendMessage("SkyWars");
+            player.setGameMode(GameMode.SURVIVAL);
+            player.setFoodLevel(20);
+            player.setHealth(20.0);
+            player.getWorld().setPVP(true);
+            player.getInventory().clear();
+
+            // ワールドにいる人数が1人だった場合スケジューラースタート
+            List<Player> players = player.getWorld().getPlayers();
+            if (players.size() <= 1) {
+                World world = player.getWorld();
+                world.getBlockAt(-476, 4, -856).setType(Material.CHEST);
             }
         }
     }
