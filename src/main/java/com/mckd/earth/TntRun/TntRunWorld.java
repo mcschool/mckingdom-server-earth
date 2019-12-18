34aafc9621175dd3888649346b5a2c33aa3b2013
Yuki-Shibanuma <smatosan@gmail.com>
2019-12-18 18:56:18 +0900

    message

--- src/main/java/com/mckd/earth/TntRun/TntRunWorld.java
+++ src/main/java/com/mckd/earth/TntRun/TntRunWorld.java
@@ -1,7 +1,16 @@
 package com.mckd.earth.TntRun;
 
 import com.mckd.earth.Earth;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.List;
 
 public class TntRunWorld implements Listener {
 
@@ -12,5 +21,52 @@ public class TntRunWorld implements Listener {
         this.plugin = plugin;
     }
 
+    @EventHandler
+    public void onPlayerChanged(PlayerChangedWorldEvent e) {
+        Player player = e.getPlayer();
+        if (!player.getWorld().getName().equals("tnt")){
+            return;
 
+        }
+        player.getInventory().clear();
+        player.setGameMode(GameMode.ADVENTURE);
+
+    }
+
+    @EventHandler
+    public void  onPlayerIntract(PlayerInteractEvent e){
+        Player player= e.getPlayer();
+        if (!player.getWorld().getName().equals("tnt")){
+            return;
+        }
+
+
+        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+           Block block = e.getClickedBlock();
+           if(block.getType() == Material.TNT){
+               this.start();
+
+           }
+           if (block.getType() == Material.REDSTONE_BLOCK) {
+               //this.fillFloor();
+           }
+        }
+    }
+
+    public void start() {
+    World world = Bukkit.getWorld("tnt");
+    world.setPVP(false);
+    List<Player> players = world.getPlayers();
+    int c = 0;
+    for (Player p: players){
+        c += 3;
+        Location location = new Location(world, 0, 0, 0);
+        location.add(c, 0,0);
+        p.teleport(location);
+
+        }
+
+
+    }
 }
+
