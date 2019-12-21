6647c9cabe6ce2a6e67c556e2fe8480de03cd50b
matoi0317 <matoi.shibanuma@gmail.com>
2019-12-21 15:49:25 +0900

    ty3

--- src/main/java/com/mckd/earth/Worlds/TypingButtle/TypingButtleWorld.java
+++ src/main/java/com/mckd/earth/Worlds/TypingButtle/TypingButtleWorld.java
@@ -2,6 +2,7 @@ package com.mckd.earth.Worlds.TypingButtle;
 
 import com.mckd.earth.Earth;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -23,6 +24,7 @@ public class TypingButtleWorld implements Listener {
     public void enterWorld(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
         if(!player.getWorld().getName().equals(this.worldname)) return;
+        player.setGameMode(GameMode.ADVENTURE);
         if(player.getWorld().getPlayers().size() == 1){
             Location location = new Location(player.getWorld(),-946, 18, 179);
             player.teleport(location);
@@ -30,9 +32,14 @@ public class TypingButtleWorld implements Listener {
         }
 
         if(player.getWorld().getPlayers().size() == 2){
-            Location location = new Location(player.getWorld(),-946, 18, 166);
-            player.teleport(location);
-            player.sendTitle(ChatColor.WHITE+ "あなたは", ChatColor.RED + "青チーム" + ChatColor.WHITE + "です", 60, 80, 60);
+            Location location1 = new Location(player.getWorld(),-946, 18, 166);
+            player.teleport(location1);
+            player.sendTitle(ChatColor.WHITE+ "あなたは", ChatColor.BLUE + "青チーム" + ChatColor.WHITE + "です", 60, 80, 60);
+        }
+        if(player.getWorld().getPlayers().size() > 2){
+            Location location2 = new Location(player.getWorld(),-946, 27, 170);
+            player.teleport(location2);
+            player.sendTitle(ChatColor.WHITE + "あなたは" + ChatColor.GREEN + "観覧車", ChatColor.WHITE + "です", 60, 80, 60);
         }
     }
 }
