992778c1113da57ebba4b01e0ab4a807fcb9b428
matoi0317 <matoi.shibanuma@gmail.com>
2019-12-21 18:07:37 +0900

    ty7

--- src/main/java/com/mckd/earth/Worlds/TypingButtle/TypingButtleWorld.java
+++ src/main/java/com/mckd/earth/Worlds/TypingButtle/TypingButtleWorld.java
@@ -6,11 +6,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.scheduler.BukkitTask;
 
 import javax.xml.soap.Text;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class TypingButtleWorld implements Listener {
+    Player playerRed;
+    Player playerBlue;
+    //public Map<String, Integer> taskIds = new HashMap<String, Integer>();
     private Earth plugin;
     String worldname = "ty";
 
@@ -30,16 +36,22 @@ public class TypingButtleWorld implements Listener {
         player.setFoodLevel(20);
         player.getWorld().setPVP(false);
         player.getInventory().clear();
+
+        //赤チーム
         if(player.getWorld().getPlayers().size() == 1){
             Location location = new Location(player.getWorld(),-946, 18, 179);
             player.teleport(location);
             player.sendTitle(ChatColor.WHITE+ "あなたは", ChatColor.RED + "赤チーム" + ChatColor.WHITE + "です", 60, 80, 60);
+            this.playerRed = player ;
         }
 
+        //青チーム
         if(player.getWorld().getPlayers().size() == 2){
-            Location location1 = new Location(player.getWorld(),-946, 18, 166);
+            Location location1 = new Location(player.getWorld(),-946,18, 166);
             player.teleport(location1);
             player.sendTitle(ChatColor.WHITE+ "あなたは", ChatColor.BLUE + "青チーム" + ChatColor.WHITE + "です", 60, 80, 60);
+            this.playerBlue = player;
+            this.Start();
         }
         if(player.getWorld().getPlayers().size() > 2){
             Location location2 = new Location(player.getWorld(),-946, 27, 170);
@@ -48,4 +60,15 @@ public class TypingButtleWorld implements Listener {
         }
     }
 
+
+    public void Start(){
+        Random r = new Random();
+        int n = r.nextInt(3);
+        String q = "a";
+        if(n == 0) {q="hello";}
+        if(n == 1) {q="goodbye";}
+        if(n == 2) {q="apple";}
+        this.playerRed.sendTitle(q,"",0,20000,0);
+        this.playerBlue.sendTitle(q,"",0,20000,0);
+    }
 }
