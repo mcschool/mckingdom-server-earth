65d66c58e73499787d538c87f249269e1255f7f8
BuildTools <unconfigured@null.spigotmc.org>
2019-12-26 16:20:39 +0900

    SkyWarsチェスト追加

--- src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWarsSchedulerChest.java
+++ src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWarsSchedulerChest.java
@@ -42,7 +42,7 @@ public class SkyWarsSchedulerChest extends BukkitRunnable {
     private void sendMessageToPlayers(World world, String msg){
         for( Player player: world.getPlayers() ){
             player.sendMessage(msg);
-            player.sendTitle(msg,"", 0,20,0);
+            player.sendTitle(msg,"", 0,10,0);
         }
     }
 }
