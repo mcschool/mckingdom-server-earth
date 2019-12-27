c5f5a94a0a289b2a694664a103a5c4fa0e4e8a84
BuildTools <unconfigured@null.spigotmc.org>
2019-12-27 18:51:33 +0900

    SkyWarsTest

--- src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
+++ src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
@@ -80,7 +80,7 @@ public class SkyWars implements Listener {
             player.getInventory().clear();
             List<Player> players = player.getWorld().getPlayers();
             // ワールドにいる人数が1人だ以上だった場合スケジューラースタート
-            if (players.size() == 1) {
+            if (players.size() == 2) {
                 new SkyWarsScheduler(this.plugin, player.getWorld()).runTaskTimer(this.plugin, 0, 20);
             }
             //ワールドに入った時にチェストを置く
