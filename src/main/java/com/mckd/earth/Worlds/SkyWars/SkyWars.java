820d96a72bea567c4495c632cb8f0580e5047e80
BuildTools <unconfigured@null.spigotmc.org>
2019-12-28 15:22:04 +0900

    test4

--- src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
+++ src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
@@ -97,6 +97,63 @@ public class SkyWars implements Listener {
                 world.getBlockAt(473, 16, -876).setType(GLASS);
                 world.getBlockAt(473, 14, -875).setType(GLASS);
                 world.getBlockAt(473, 17, -875).setType(GLASS);
+
+                //島1
+                world.getBlockAt(471, 11, -875).setType(CHEST);
+                world.getBlockAt(469, 11, -879).setType(CHEST);
+                world.getBlockAt(472, 6, -872).setType(CHEST);
+                //島2
+                world.getBlockAt(430, 11, -877).setType(CHEST);
+                world.getBlockAt(425, 11, -875).setType(CHEST);
+                world.getBlockAt(420, 7, -871).setType(CHEST);
+                //氷野島
+                world.getBlockAt(438, 16, -901).setType(CHEST);
+                world.getBlockAt(455, 14, -857).setType(CHEST);
+                //火の島
+                world.getBlockAt(447, 13, -901).setType(CHEST);
+                world.getBlockAt(440, 16, -900).setType(CHEST);
+
+                //島1チェスト中身
+                Chest chestisland1of1 = (Chest) world.getBlockAt(471, 11, -875).getState();
+                Inventory invisland1of1 = chestisland1of1.getInventory();
+                invisland1of1.clear();
+                invisland1of1.setItem(1, new ItemStack(STONE, 24));
+                invisland1of1.setItem(18, new ItemStack(WOOD_SWORD));
+                Chest chestisland1of2 = (Chest) world.getBlockAt(469, 11, -879).getState();
+                Inventory invisland1of2 = chestisland1of2.getInventory();
+                invisland1of2.clear();
+                invisland1of2.setItem(5, new ItemStack(WOOD, 32));
+                invisland1of2.setItem(20, new ItemStack(EGG, 16));
+                Chest chestisland1of3 = (Chest) world.getBlockAt(472, 11, -872).getState();
+                Inventory invisland1of3 = chestisland1of3.getInventory();
+                invisland1of3.clear();
+                invisland1of3.setItem(8, new ItemStack(LEATHER_CHESTPLATE));
+                invisland1of3.setItem(22, new ItemStack(LEATHER_HELMET));
+
+                //島2チェスト中身
+                Chest chestisland2of1 = (Chest) world.getBlockAt(430, 11, -877).getState();
+                Inventory invisland2of1 = chestisland2of1.getInventory();
+                invisland2of1.clear();
+                invisland2of1.setItem(1, new ItemStack(STONE, 24));
+                invisland2of1.setItem(18, new ItemStack(WOOD_SWORD));
+                Chest chestisland2of2 = (Chest) world.getBlockAt(425, 11, -875).getState();
+                Inventory invisland2of2 = chestisland2of2.getInventory();
+                invisland2of2.clear();
+                invisland2of2.setItem(5, new ItemStack(WOOD, 32));
+                invisland2of2.setItem(20, new ItemStack(EGG, 16));
+                Chest chestisland2of3 = (Chest) world.getBlockAt(420, 7, -871).getState();
+                Inventory invisland2of3 = chestisland2of3.getInventory();
+                invisland2of2.clear();
+                invisland2of3.setItem(8, new ItemStack(LEATHER_CHESTPLATE));
+                invisland2of3.setItem(22, new ItemStack(LEATHER_HELMET));
+
+            //ワールドに入った時にプレイヤーをテレポートさせる
+                player.sendMessage("test1");
+                Location location = new Location(player.getWorld(), 473.494, 15, -874.500);
+                player.teleport(location);
+                player.sendMessage(toString().valueOf(players.size()));
+                player.sendMessage("test2");
+
             }
 
             if (players.size() == 2) {
@@ -114,7 +171,7 @@ public class SkyWars implements Listener {
                 world2.getBlockAt(427, 17, -875).setType(GLASS);
             }
 
-            //ワールドに入った時にチェストを置く
+           /* //ワールドに入った時にチェストを置く
             if (players.size() == 1) {
                 World worldchest = player.getWorld();
                 //島1
@@ -173,7 +230,7 @@ public class SkyWars implements Listener {
                 player.teleport(location);
                 player.sendMessage(toString().valueOf(players.size()));
                 player.sendMessage("test2");
-            }
+            }*/
             if (players.size() == 2) {
                 Location location = new Location(player.getWorld(), 427.494, 15, -874.491);
                 player.teleport(location);
