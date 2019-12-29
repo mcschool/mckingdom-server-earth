ce91844c908e23811eaceefbca3c2a8a9b4bf159
BuildTools <unconfigured@null.spigotmc.org>
2019-12-29 10:09:06 +0900

    SkyWarsチェスト中身

--- src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
+++ src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
@@ -99,7 +99,14 @@ public class SkyWars implements Listener {
                 world.getBlockAt(473, 17, -875).setType(Material.GLASS);
 
                 this.spawnChest(new Location(world, 471,11,-875), 0);
+                this.spawnChest(new Location(world, 425,11,-875), 0);
+
                 this.spawnChest(new Location(world, 469, 11, -879), 1);
+                this.spawnChest(new Location(world, 431, 11, -879), 1);
+
+                this.spawnChest(new Location(world, 472, 6, -872), 1);
+                this.spawnChest(new Location(world, 420, 7, -871), 1);
+
               /*  //島1
                 world.getBlockAt(471, 11, -875).setType(Material.CHEST);
                 world.getBlockAt(469, 11, -879).setType(Material.CHEST);
@@ -181,66 +188,7 @@ public class SkyWars implements Listener {
                 world2.getBlockAt(427, 17, -875).setType(Material.GLASS);
             }
 
-           /* //ワールドに入った時にチェストを置く
-            if (players.size() == 1) {
-                World worldchest = player.getWorld();
-                //島1
-                worldchest.getBlockAt(471, 11, -875).setType(CHEST);
-                worldchest.getBlockAt(469, 11, -879).setType(CHEST);
-                worldchest.getBlockAt(472, 6, -872).setType(CHEST);
-                //島2
-                worldchest.getBlockAt(430, 11, -877).setType(CHEST);
-                worldchest.getBlockAt(425, 11, -875).setType(CHEST);
-                worldchest.getBlockAt(420, 7, -871).setType(CHEST);
-                //氷野島
-                worldchest.getBlockAt(438, 16, -901).setType(CHEST);
-                worldchest.getBlockAt(455, 14, -857).setType(CHEST);
-                //火の島
-                worldchest.getBlockAt(447, 13, -901).setType(CHEST);
-                worldchest.getBlockAt(440, 16, -900).setType(CHEST);
 
-                //島1チェスト中身
-                Chest chestisland1of1 = (Chest) worldchest.getBlockAt(471, 11, -875).getState();
-                Inventory invisland1of1 = chestisland1of1.getInventory();
-                invisland1of1.clear();
-                invisland1of1.setItem(1, new ItemStack(STONE, 24));
-                invisland1of1.setItem(18, new ItemStack(WOOD_SWORD));
-                Chest chestisland1of2 = (Chest) worldchest.getBlockAt(469, 11, -879).getState();
-                Inventory invisland1of2 = chestisland1of2.getInventory();
-                invisland1of2.clear();
-                invisland1of2.setItem(5, new ItemStack(WOOD, 32));
-                invisland1of2.setItem(20, new ItemStack(EGG, 16));
-                Chest chestisland1of3 = (Chest) worldchest.getBlockAt(472, 11, -872).getState();
-                Inventory invisland1of3 = chestisland1of3.getInventory();
-                invisland1of3.clear();
-                invisland1of3.setItem(8, new ItemStack(LEATHER_CHESTPLATE));
-                invisland1of3.setItem(22, new ItemStack(LEATHER_HELMET));
-
-                //島2チェスト中身
-                Chest chestisland2of1 = (Chest) worldchest.getBlockAt(430, 11, -877).getState();
-                Inventory invisland2of1 = chestisland2of1.getInventory();
-                invisland2of1.clear();
-                invisland2of1.setItem(1, new ItemStack(STONE, 24));
-                invisland2of1.setItem(18, new ItemStack(WOOD_SWORD));
-                Chest chestisland2of2 = (Chest) worldchest.getBlockAt(425, 11, -875).getState();
-                Inventory invisland2of2 = chestisland2of2.getInventory();
-                invisland2of2.clear();
-                invisland2of2.setItem(5, new ItemStack(WOOD, 32));
-                invisland2of2.setItem(20, new ItemStack(EGG, 16));
-                Chest chestisland2of3 = (Chest) worldchest.getBlockAt(420, 7, -871).getState();
-                Inventory invisland2of3 = chestisland2of3.getInventory();
-                invisland2of2.clear();
-                invisland2of3.setItem(8, new ItemStack(LEATHER_CHESTPLATE));
-                invisland2of3.setItem(22, new ItemStack(LEATHER_HELMET));
-            }
-            //ワールドに入った時にプレイヤーをテレポートさせる
-            if (players.size() ==0) {
-                player.sendMessage("test1");
-                Location location = new Location(player.getWorld(), 473.494, 15, -874.500);
-                player.teleport(location);
-                player.sendMessage(toString().valueOf(players.size()));
-                player.sendMessage("test2");
-            }*/
             if (players.size() == 2) {
                 Location location = new Location(player.getWorld(), 427.494, 15, -874.491);
                 player.teleport(location);
@@ -265,6 +213,10 @@ public class SkyWars implements Listener {
         if (type == 1) {
             inv.setItem(5, new ItemStack(WOOD, 32));
             inv.setItem(20, new ItemStack(EGG, 16));
+        }
+        if (type == 2) {
+            inv.setItem(9,new ItemStack(LEATHER_CHESTPLATE));
+            inv.setItem(25,new ItemStack(LEATHER_HELMET));
         }
     }
 
