aea1c6465ee7e65bbb3b504c1c41fad0a413a0f9
BuildTools <unconfigured@null.spigotmc.org>
2020-01-25 14:33:51 +0900

    SkyWarsTest7

--- src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
+++ src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
@@ -91,9 +91,11 @@ public class SkyWars implements Listener {
             player.getInventory().clear();
             List<Player> players = player.getWorld().getPlayers();
             // ワールドにいる人数が1人だ以上だった場合スケジューラースタート
-            if (players.size() == 1) {
+            if (players.size() == 2) {
                 new SkyWarsScheduler(this.plugin, player.getWorld()).runTaskTimer(this.plugin, 0, 20);
                 new SkyWarsSchedulerChest(this.plugin, player.getWorld()).runTaskTimer(this.plugin, 0, 20);
+            }
+            if(players.size() == 1){
                 World world = player.getWorld();
                 //1人目のガラス
                 world.getBlockAt(473, 15, -874).setType(Material.GLASS);
@@ -122,61 +124,7 @@ public class SkyWars implements Listener {
                 this.spawnChest(new Location(world, 440, 16, -900), 4);
                 this.spawnChest(new Location(world, 455, 14, -857), 4);
 
-              /*  //島1
-                world.getBlockAt(471, 11, -875).setType(Material.CHEST);
-                world.getBlockAt(469, 11, -879).setType(Material.CHEST);
-                world.getBlockAt(472, 6, -872).setType(Material.CHEST);
-                //島2
-                world.getBlockAt(430, 11, -877).setType(Material.CHEST);
-                world.getBlockAt(425, 11, -875).setType(Material.CHEST);
-                world.getBlockAt(420, 7, -871).setType(Material.CHEST);
-                //氷野島
-                world.getBlockAt(438, 16, -901).setType(Material.CHEST);
-                world.getBlockAt(455, 14, -857).setType(Material.CHEST);
-                //火の島
-                world.getBlockAt(447, 13, -901).setType(Material.CHEST);
-                world.getBlockAt(440, 16, -900).setType(Material.CHEST);
-                player.sendMessage("test 10");
 
-                player.sendMessage("test 10");
-
-                //島1チェスト中身
-                Chest chestisland1of1 = (Chest) world.getBlockAt(471, 11, -875).getState();
-                Inventory invisland1of1 = chestisland1of1.getInventory();
-                invisland1of1.clear();
-                invisland1of1.setItem(1, new ItemStack(STONE, 24));
-                invisland1of1.setItem(18, new ItemStack(WOOD_SWORD));
-                Chest chestisland1of2 = (Chest) world.getBlockAt(469, 11, -879).getState();
-                Inventory invisland1of2 = chestisland1of2.getInventory();
-                invisland1of2.clear();
-                invisland1of2.setItem(5, new ItemStack(WOOD, 32));
-                invisland1of2.setItem(20, new ItemStack(EGG, 16));
-                Chest chestisland1of3 = (Chest) world.getBlockAt(472, 6, -872).getState();
-                Inventory invisland1of3 = chestisland1of3.getInventory();
-                invisland1of3.clear();
-                invisland1of3.setItem(8, new ItemStack(LEATHER_CHESTPLATE));
-                invisland1of3.setItem(22, new ItemStack(LEATHER_HELMET));
-                player.sendMessage("test11");
-
-                player.sendMessage("test 11");
-
-                //島2チェスト中身
-                Chest chestisland2of1 = (Chest) world.getBlockAt(430, 11, -877).getState();
-                Inventory invisland2of1 = chestisland2of1.getInventory();
-                invisland2of1.clear();
-                invisland2of1.setItem(1, new ItemStack(STONE, 24));
-                invisland2of1.setItem(18, new ItemStack(WOOD_SWORD));
-                Chest chestisland2of2 = (Chest) world.getBlockAt(425, 11, -875).getState();
-                Inventory invisland2of2 = chestisland2of2.getInventory();
-                invisland2of2.clear();
-                invisland2of2.setItem(5, new ItemStack(WOOD, 32));
-                invisland2of2.setItem(20, new ItemStack(EGG, 16));
-                Chest chestisland2of3 = (Chest) world.getBlockAt(420, 7, -871).getState();
-                Inventory invisland2of3 = chestisland2of3.getInventory();
-                invisland2of2.clear();
-                invisland2of3.setItem(8, new ItemStack(LEATHER_CHESTPLATE));
-                invisland2of3.setItem(22, new ItemStack(LEATHER_HELMET));
-                 */
 
 
                 //ワールドに入った時にプレイヤーをテレポートさせる
