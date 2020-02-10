f379e2aa8491613606f3fb26db67e910e3faf59c
BuildTools <unconfigured@null.spigotmc.org>
2020-02-10 18:03:40 +0900

    SkyWars

--- src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
+++ src/main/java/com/mckd/earth/Worlds/SkyWars/SkyWars.java
@@ -71,7 +71,7 @@ public class SkyWars implements Listener {
                             public void run() {
                                 player1.performCommand("mvtp world");
                                 //player1.performCommand("WorldRestorer load SkyWars");
-                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),"reload");
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),"WorldRestorer load SkyWars2");
                             }
                         }.runTaskLater(this.plugin, 100);
                     } else {
