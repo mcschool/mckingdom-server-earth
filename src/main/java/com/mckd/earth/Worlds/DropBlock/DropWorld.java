c6ec6eec2b6c23fbdf9b4991b15088e61184d7e4
matoi0317 <matoi.shibanuma@gmail.com>
2020-03-02 17:53:02 +0900

    f2

--- src/main/java/com/mckd/earth/Worlds/DropBlock/DropWorld.java
+++ src/main/java/com/mckd/earth/Worlds/DropBlock/DropWorld.java
@@ -3,10 +3,17 @@ package com.mckd.earth.Worlds.DropBlock;
 import com.mckd.earth.Earth;
 import org.bukkit.GameMode;
 import org.bukkit.WeatherType;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.util.Vector;
 
 public class DropWorld implements Listener {
     private Earth plugin;
@@ -27,5 +34,28 @@ public class DropWorld implements Listener {
         player.setFoodLevel(20);
         player.getWorld().setPVP(false);
     }
+
+    @EventHandler
+    public void onExplode(EntityExplodeEvent event){
+        for (Block blocks : event.blockList()) {
+            event.setCancelled(true);
+            float x = (float) (-1.0D + 2.5 * Math.random());
+            float y = (float) (-2.0D + 3.5 * Math.random());
+            float z = (float) (-1.0D + 2.5 * Math.random());
+
+            FallingBlock fallingBlock = blocks.getWorld().spawnFallingBlock(blocks.getLocation(), blocks.getType(), blocks.getData());
+            fallingBlock.setDropItem(false);
+            fallingBlock.setVelocity(new Vector(x, y, z));
+
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void EntityChangeBlock(EntityChangeBlockEvent event){
+        if (event.getEntityType() == EntityType.FALLING_BLOCK) {
+            event.setCancelled(true);
+        }
+    }
+
 }
 
