dceebac571bbb3684dbf4d6bf17189619f1eeca0
matoi0317 <matoi.shibanuma@gmail.com>
2020-03-02 18:03:38 +0900

    f3

--- src/main/java/com/mckd/earth/Worlds/DropBlock/DropWorld.java
+++ src/main/java/com/mckd/earth/Worlds/DropBlock/DropWorld.java
@@ -1,7 +1,9 @@
 package com.mckd.earth.Worlds.DropBlock;
 
 import com.mckd.earth.Earth;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.WeatherType;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
@@ -14,6 +16,8 @@ import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.util.Vector;
+
+import java.util.Random;
 
 public class DropWorld implements Listener {
     private Earth plugin;
@@ -33,29 +37,13 @@ public class DropWorld implements Listener {
         player.setHealth(20.0);
         player.setFoodLevel(20);
         player.getWorld().setPVP(false);
+
+        Location location = new Location(player.getWorld(), 0,0,0);
+        Random random = new Random();
+        location.setX(random.nextInt(10));
+        location.setY(random.nextInt(10));
+        location.setZ(random.nextInt(10));
+        player.teleport(location);
     }
-
-    @EventHandler
-    public void onExplode(EntityExplodeEvent event){
-        for (Block blocks : event.blockList()) {
-            event.setCancelled(true);
-            float x = (float) (-1.0D + 2.5 * Math.random());
-            float y = (float) (-2.0D + 3.5 * Math.random());
-            float z = (float) (-1.0D + 2.5 * Math.random());
-
-            FallingBlock fallingBlock = blocks.getWorld().spawnFallingBlock(blocks.getLocation(), blocks.getType(), blocks.getData());
-            fallingBlock.setDropItem(false);
-            fallingBlock.setVelocity(new Vector(x, y, z));
-
-        }
-    }
-
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void EntityChangeBlock(EntityChangeBlockEvent event){
-        if (event.getEntityType() == EntityType.FALLING_BLOCK) {
-            event.setCancelled(true);
-        }
-    }
-
 }
 
