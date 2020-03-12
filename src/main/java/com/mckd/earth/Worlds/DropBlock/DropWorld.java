2746679ce4e520249fa8f14619bba5ad7626c514
matoi0317 <matoi.shibanuma@gmail.com>
2020-03-12 09:20:10 +0900

    d2

--- src/main/java/com/mckd/earth/Worlds/DropBlock/DropWorld.java
+++ src/main/java/com/mckd/earth/Worlds/DropBlock/DropWorld.java
@@ -1,22 +1,22 @@
 package com.mckd.earth.Worlds.DropBlock;
 
 import com.mckd.earth.Earth;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.WeatherType;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 
 import java.util.Random;
@@ -39,7 +39,28 @@ public class DropWorld implements Listener {
         player.setHealth(20.0);
         player.setFoodLevel(20);
         player.getWorld().setPVP(false);
+    }
 
+    @EventHandler
+    public void signClick(PlayerInteractEvent event){
+        Player player = event.getPlayer();
+        if (!player.getWorld().getName().equals(this.worldname)){
+            return;
+        }
+        Block block = event.getClickedBlock();
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && block.getType() == Material.SIGN_POST) {
+            Sign sign;
+            sign = (Sign) block.getState();
+            String line = sign.getLine(1);
+            String line2 = sign.getLine(2);
+            player.sendMessage("あなたの前世は王子様orお姫様だと思う");
+            if (line.equals("はい")){
+                player.sendMessage("あなたの前世は豚です");
+            }
+            if (line.equals("いいえ")){
+                player.sendMessage("朝ごはんは毎日食べる");
+            }
+        }
     }
 }
 
