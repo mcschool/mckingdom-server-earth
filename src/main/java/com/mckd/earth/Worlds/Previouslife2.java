f4bfbbdad25103b3e40b427fc1b78b39074fbe1d
BuildTools <unconfigured@null.spigotmc.org>
2020-03-21 10:29:38 +0900

    Previouslife2_2

--- src/main/java/com/mckd/earth/Worlds/Previouslife2.java
+++ src/main/java/com/mckd/earth/Worlds/Previouslife2.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -18,6 +19,8 @@ import java.util.Random;
 public class Previouslife2 implements Listener {
     Player playerRed;
     String worldName = "Previouslife2";
+    String Correct_answer;
+    String Incorrect_answer;
 
 
     public Previouslife2(Earth plugin) {
@@ -39,13 +42,14 @@ public class Previouslife2 implements Listener {
             player.sendMessage("これからあなたの前世診断を始めます");
 
             this.Start();
+            this.Next();
         }
     }
 
 
     public void Start() {
         Random r = new Random();
-        int n = r.nextInt(10);
+        int n = r.nextInt(7);
         String a = "a";
         String b = "b";
         String c = "c";
@@ -55,6 +59,7 @@ public class Previouslife2 implements Listener {
         String g = "g";
         if (n == 0) {
             a = "前世は王様だと思う";
+
         }
         if (n == 1) {
             b = "前世は毎日朝ごはん食べている";
@@ -82,10 +87,59 @@ public class Previouslife2 implements Listener {
         this.playerRed.sendTitle(e, "", 0, 20000, 0);
         this.playerRed.sendTitle(f, "", 0, 20000, 0);
         this.playerRed.sendTitle(g, "", 0, 20000, 0);
+    }
+
+    public void Next(){
+        Random r = new Random();
+        int n = r.nextInt(7);
+        String a = "a";
+        String b = "b";
+        String c = "c";
+        String d = "d";
+        String e = "e";
+        String f = "f";
+        String g = "g";
+        if (n == 0) {
+            a = "前世は王様だと思う222";
+        }
+        if (n == 1) {
+            b = "前世は毎日朝ごはん食べている22";
+        }
+        if (n == 2) {
+            c = "前世は水をよく飲む222";
+        }
+        if (n == 3) {
+            d = "勉強が得意222";
+        }
+        if (n == 4) {
+            e = "空を見るのが好き222";
+        }
+        if (n == 5) {
+            f = "運動が得意2222";
+        }
+        if (n == 6) {
+            g = "肉と魚だったら肉の方が好き222";
+        }
+
+        this.playerRed.sendTitle(a, "", 0, 20000, 0);
+        this.playerRed.sendTitle(b, "", 0, 20000, 0);
+        this.playerRed.sendTitle(c, "", 0, 20000, 0);
+        this.playerRed.sendTitle(d, "", 0, 20000, 0);
+        this.playerRed.sendTitle(e, "", 0, 20000, 0);
+        this.playerRed.sendTitle(f, "", 0, 20000, 0);
+        this.playerRed.sendTitle(g, "", 0, 20000, 0);
+    }
 
 
+    public void Correct_answer() {
+        String c = "yes";
+        this.Correct_answer = c;
+    }
 
 
+    public void Incorrect_answer(){
+        String i = "no";
+        this.Incorrect_answer = i;
     }
 
     @EventHandler
@@ -99,11 +153,58 @@ public class Previouslife2 implements Listener {
                 String line = sign.getLine(1);
                 if (line.equals("Yes")) {
                     player.chat("yes");
+
                 }
                 if (line.equals("No")) {
                     player.chat("no");
                 }
             }
+        }
+    }
+
+
+    @EventHandler
+    public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
+        if (event.getPlayer().getWorld().getName().equals("Previouslife")) {
+            Player player = event.getPlayer();
+            String mes = event.getMessage();
+            String question = this.Correct_answer;
+            if (mes.equals(question)) {
+                if (player == this.playerRed) {
+                    Random r = new Random();
+                    int n = r.nextInt(3);
+                    if (n ==0){
+                        this.Next();
+                    }
+                    if (n == 1) {
+                        this.Start();
+                    }
+                }
+            }
+            event.setCancelled(true);
+        }
+    }
+
+
+    @EventHandler
+    public void onAsyncPlayerChatEvent2(AsyncPlayerChatEvent event) {
+        if (event.getPlayer().getWorld().getName().equals("Previouslife")) {
+            Player player = event.getPlayer();
+            String mes = event.getMessage();
+            String question = this.Incorrect_answer;
+            if (mes.equals(question)) {
+                if (player == this.playerRed) {
+                    Random r = new Random();
+                    int n = r.nextInt(3);
+                    if (n ==0){
+                        this.Next();
+                    }
+                    if (n == 1) {
+                        this.Start();
+                    }
+                }
+            }
+            event.setCancelled(true);
         }
     }
 }
