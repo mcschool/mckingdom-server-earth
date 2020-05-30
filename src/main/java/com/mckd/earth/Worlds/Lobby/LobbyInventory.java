b74e385094f2136aa01f26fd32348a8a0b9ea8ff
matoi0317 <matoi.shibanuma@gmail.com>
2020-05-30 14:01:50 +0900

    test

--- src/main/java/com/mckd/earth/Worlds/Lobby/LobbyInventory.java
+++ src/main/java/com/mckd/earth/Worlds/Lobby/LobbyInventory.java
@@ -118,7 +118,7 @@ public class LobbyInventory {
         ItemMeta flowerMeta = flower.getItemMeta();
         tntMeta.setDisplayName("生活サーバー");
         List<String> flowerLores = new ArrayList<>();
-        flowerLores.add("みんなでのんびり生活サーバー");
+        flowerLores.add("メンテ中です。16:30くらいまで入れません");
         flowerMeta.setLore(flowerLores);
         flower.setItemMeta(flowerMeta);
         inv.setItem(25, flower);
