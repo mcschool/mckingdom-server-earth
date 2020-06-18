ffa7b35ec3fa168436d9a7b79dc0b75545b71a4d
matoi0317 <matoi.shibanuma@gmail.com>
2020-06-18 17:23:02 +0900

    tntrun

--- src/main/java/com/mckd/earth/TntRun/TntRunWorld.java
+++ src/main/java/com/mckd/earth/TntRun/TntRunWorld.java
@@ -23,6 +23,7 @@ public class TntRunWorld implements Listener {
 
     Earth plugin;
     String status = "wait";
+    String woldname = "tntrun";
 
     public TntRunWorld(Earth plugin) {
         this.plugin = plugin;
@@ -40,7 +41,6 @@ public class TntRunWorld implements Listener {
         World world = player.getWorld();
         Location location = new Location(world,555,4,434);
         player.teleport(location);
-
     }
 
     @EventHandler
@@ -61,151 +61,19 @@ public class TntRunWorld implements Listener {
         }
     }
 
-    /*
     @EventHandler
-    public void onPlayerIntract(PlayerInteractEvent e) {
-        Player player = e.getPlayer();
-        if (!player.getWorld().getName().equals("tntrun")) {
+    public void onPlayerMoveEvent(PlayerMoveEvent event){
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+        if (event.getPlayer().getWorld().getName().equals(this.woldname)){
             return;
         }
-
-        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-            Block block = e.getClickedBlock();
-            if (block.getType() == Material.TNT) {
-                player.sendMessage("Tnt is clicked");
-                this.start();
-            }
-            if (block.getType() == Material.REDSTONE_BLOCK) {
-                player.sendMessage("Redstone is clicked");
-                this.fillFloor(player);
-            }
+        Location location = event.getPlayer().getLocation().clone().subtract(0,-1,0);
+        Block block = location.getBlock();
+        if (block.getType() == Material.TNT){
+            world.getBlockAt(location).setType(Material.AIR);
         }
     }
-
-    public void start() {
-        World world = Bukkit.getWorld("tntrun");
-        world.setPVP(false);
-        List<Player> players = world.getPlayers();
-        int c = 0;
-        for (Player p : players) {
-            c += 3;
-            Location location = new Location(world, 0, 0, 0);
-            location.add(c, 0, 0);
-            p.teleport(location);
-        }
-    }
-
-    public void fillFloor(Player player) {
-        World world = Bukkit.getWorld("tntrun");
-        Location location = player.getLocation();
-        location.add(0, 10, 0);
-        Double nowX = location.getX();
-        Double nowZ = location.getZ();
-        for (int x = 0; x < 30; x++) {
-            location.setX(nowX + x);
-            for (int z = 0; z < 30; z++) {
-                location.setZ(nowZ + z);
-                world.getBlockAt(location).setType(Material.TNT);
-            }
-        }
-    }
-
-
-    public void fillFirstFloor() {
-        World world = Bukkit.getWorld("tntrun");
-        Location location = new Location(world, 0, 0, 0);
-        Double nowX = location.getX();
-        Double nowZ = location.getZ();
-        for (int x = 0; x < 30 ;x++){
-            location.setX(nowX + x);
-            for (int z = 0; x < 30; z++) {
-                location.setZ(nowZ + z);
-                world.getBlockAt(location).setType(Material.TNT);
-            }
-        }
-    }
-
-    public void fillSecondFloor() {
-        World world = Bukkit.getWorld("tntrun");
-        Location location = new Location(world,0,0,0);
-        location.add(0, 0, 0);
-        Double nowX = location.getX();
-        Double nowZ = location.getZ();
-        for (int X=0; X<30; X++) {
-            location.setX(nowX + X);
-            for (int z=0; z<30; z++) {
-                location.setZ (nowZ + z);
-                world.getBlockAt(location).setType(Material.TNT);
-
-            }
-
-
-        }
-    }
-    public void fillThirdFloor() {
-        World world =Bukkit.getWorld("tntrun");
-        Location location = new Location(world, 0,0,0);
-        location.add(0, 0, 0);
-        Double nowX = location.getX();
-        Double nowZ = location.getZ();
-        for (int X=0; X<30; X++) {
-            location.setX(nowX + X);
-            for (int Z=0; Z<30; Z++) {
-                location.setX(nowX + Z);
-
-            }
-
-        }
-
-
-
-    }
-
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent e) {
-        Player player = e.getPlayer();
-        if (!player.getWorld().getName().equals("tntrun")) {
-            return;
-        }
-
-        Location location = player.getLocation();
-        Double nowY = location.getY();
-        location.setY(nowY - 1);
-        Block block = player.getWorld().getBlockAt(location);
-        if (block.getType() == Material.TNT) {
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    block.setType(Material.AIR);
-                }
-            }.runTaskLater(this.plugin, 5);
-
-        }
-        if (nowY < 3 && player.getGameMode() == GameMode.ADVENTURE) {
-            player.sendMessage("Change spectator");
-            player.setGameMode(GameMode.SPECTATOR);
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    player.sendMessage("run mvtp");
-                    player.performCommand("mvtp world");
-                }
-            }.runTaskLater(this.plugin, 100);
-        }
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent e) {
-        Player player = e.getPlayer();
-        if (!player.getWorld().getName().equals("tntrun")) {
-            return;
-        }
-        if (player.getGameMode() != GameMode.CREATIVE) {
-            e.setCancelled(true);
-
-        }
-    }
-    */
 }
 
 
