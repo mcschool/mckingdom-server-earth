7ba9756e2fa6d54b2468c1e9835dab030d55e685
matoi0317 <matoi.shibanuma@gmail.com>
2020-08-29 15:34:59 +0900

    s

--- src/main/java/com/mckd/earth/Worlds/water/water.java
+++ src/main/java/com/mckd/earth/Worlds/water/water.java
@@ -2,12 +2,9 @@ package com.mckd.earth.Worlds.water;
 
 import com.mckd.earth.Earth;
 import org.bukkit.*;
-<<<<<<< HEAD
 import org.bukkit.block.Block;
-=======
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Item;
->>>>>>> 4ae1adf9b4c94a5ce68018950ac20475e6cb4f69
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -34,14 +31,14 @@ public class water implements Listener {
     @EventHandler
     public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
-        player.sendMessage("You are at "+player.getWorld().getName());
+        player.sendMessage("You are at " + player.getWorld().getName());
         if (!player.getWorld().getName().equals(this.worldName)) {
             return;
         }
         player.setFoodLevel(20);
         player.setHealth(20.0);
         player.getInventory().clear();
-        Location location = new Location(player.getWorld(), 483.500,95,-781.500);
+        Location location = new Location(player.getWorld(), 483.500, 95, -781.500);
         player.teleport(location);
 
         ItemStack itemStack = new ItemStack(Material.SNOW_BALL);
@@ -53,10 +50,10 @@ public class water implements Listener {
     }
 
     @EventHandler
-    public void BlockBreakEvent(BlockBreakEvent event){
-            if (event.getPlayer().getGameMode() == GameMode.SURVIVAL){
-                event.setCancelled(true);
-            }
+    public void BlockBreakEvent(BlockBreakEvent event) {
+        if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
@@ -85,4 +82,5 @@ public class water implements Listener {
             }
         }
     }
+}
 
