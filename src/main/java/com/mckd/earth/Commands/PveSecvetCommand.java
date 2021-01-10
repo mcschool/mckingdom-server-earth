5e5ed9b41e70d7bbc82961c41f6197b4eeded11d
BuildTools <unconfigured@null.spigotmc.org>
2021-01-10 18:58:42 +0900

    test7

--- src/main/java/com/mckd/earth/Commands/PveSecvetCommand.java
+++ src/main/java/com/mckd/earth/Commands/PveSecvetCommand.java
@@ -10,25 +10,25 @@ import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 
 public class PveSecvetCommand {
-    public static boolean command(CommandSender sender, Command command, String label, String args[]) {
-        Player player = null;
+    public static boolean command(CommandSender sender, Command command,Player player, String label, String args[]) {
         player.sendMessage("test1");
-        //if (player.getWorld().getName().equals("pve")) {
-        player.sendMessage("test2");
-        if (sender instanceof Player) {
-            player = (Player) sender;
-            ScoreboardManager sbm = Bukkit.getScoreboardManager();
-            player.sendMessage("test3");
-            Scoreboard sb = sbm.getMainScoreboard();
-            Objective obj = ((Scoreboard) sb).getObjective("point");
-            player.sendMessage("test4");
-            if (obj != null) {
-                player.sendMessage("test5");
-                Score score = obj.getScore(player.getDisplayName());
-                int point = (int) score.getScore();
-                player.sendMessage("test6");
-                score.setScore(point + 2000);
-                player.setScoreboard(sb);
+        if (player.getWorld().getName().equals("pve")) {
+            player.sendMessage("test2");
+            if (sender instanceof Player) {
+                player = (Player) sender;
+                ScoreboardManager sbm = Bukkit.getScoreboardManager();
+                player.sendMessage("test3");
+                Scoreboard sb = sbm.getMainScoreboard();
+                Objective obj = ((Scoreboard) sb).getObjective("point");
+                player.sendMessage("test4");
+                if (obj != null) {
+                    player.sendMessage("test5");
+                    Score score = obj.getScore(player.getDisplayName());
+                    int point = (int) score.getScore();
+                    player.sendMessage("test6");
+                    score.setScore(point + 2000);
+                    player.setScoreboard(sb);
+                }
             }
         }
 
