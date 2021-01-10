33122e18503400a57dd779ba4a48c1d2278119e7
BuildTools <unconfigured@null.spigotmc.org>
2021-01-10 18:16:04 +0900

    test1

--- src/main/java/com/mckd/earth/Commands/PveSecvetCommand.java
+++ src/main/java/com/mckd/earth/Commands/PveSecvetCommand.java
@@ -1,6 +1,5 @@
 package com.mckd.earth.Commands;
 
-import com.sun.org.apache.xml.internal.utils.ObjectVector;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,20 +10,22 @@ import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 
 public class PveSecvetCommand {
-    public  static boolean command(CommandSender sender, Command command, String label, String args[]){
+    public  static boolean command(CommandSender sender, Command command, String label, String args[]) {
         Player player = null;
-        if (sender instanceof Player) {
-            player = (Player) sender;
-            ScoreboardManager sbm = Bukkit.getScoreboardManager();
-            Scoreboard sb = sbm.getMainScoreboard();
-            Objective obj = ((Scoreboard) sb).getObjective("point");
-            if( obj !=null){
-                Score score =obj.getScore(player.getDisplayName());
-                int point = (int)score.getScore();
-                score.setScore(point+2000);
-                player.setScoreboard(sb);
+        if (player.getWorld().getName().equals("pve")) {
+            if (sender instanceof Player) {
+                player = (Player) sender;
+                ScoreboardManager sbm = Bukkit.getScoreboardManager();
+                Scoreboard sb = sbm.getMainScoreboard();
+                Objective obj = ((Scoreboard) sb).getObjective("point");
+                if (obj != null) {
+                    Score score = obj.getScore(player.getDisplayName());
+                    int point = (int) score.getScore();
+                    score.setScore(point + 2000);
+                    player.setScoreboard(sb);
+                }
             }
+            return true;
         }
-        return  true;
     }
 }
