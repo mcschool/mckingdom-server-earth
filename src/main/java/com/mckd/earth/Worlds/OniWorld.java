4c7d7acfbed8cbacb15580dba4f6244ac8e5dc7e
BuildTools <unconfigured@null.spigotmc.org>
2021-11-07 16:35:55 +0900

    test

--- src/main/java/com/mckd/earth/Worlds/OniWorld.java
+++ src/main/java/com/mckd/earth/Worlds/OniWorld.java
@@ -19,6 +19,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scoreboard.Team;
 import ru.tehkode.permissions.PermissionGroup;
 import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
@@ -31,7 +32,8 @@ import java.util.UUID;
 public class OniWorld implements Listener {
     private Earth plugin;
     String worldName = "oni";
-    UUID oni;
+    UUID firstoni;
+    Team oni;
 
 
     public OniWorld(Earth plugin) {
@@ -74,7 +76,7 @@ public class OniWorld implements Listener {
 
         player.sendMessage(String.valueOf(result));
 
-        this.oni = result;
+        this.firstoni = result;
 
         //player.sendMessage(String.valueOf(UniqueId.get(i)));
 
@@ -99,15 +101,23 @@ public class OniWorld implements Listener {
 
             if(line.equals("test")) {
                 this.addUniqueId(player);
+
             }
             if (line.equals("oni")){
-                if (e.getPlayer().getUniqueId() == oni){
+                if (e.getPlayer().getUniqueId() == firstoni){
                     player.sendMessage("あなたは鬼です");
-                    player.sendMessage(String.valueOf(this.oni));
+                    player.sendMessage(String.valueOf(this.firstoni));
                 } else {
                     player.sendMessage("あなたは鬼じゃないです");
-                    player.sendMessage(String.valueOf(this.oni));
+                    player.sendMessage(String.valueOf(this.firstoni));
                 }
+            }
+            if (line.equals("team")){
+                this.oni = (Team) player;
+                player.sendMessage(String.valueOf(this.oni));
+            }
+            if (line.equals("noteam")){
+                player.sendMessage(String.valueOf(this.oni));
             }
         }
     }
@@ -123,26 +133,9 @@ public class OniWorld implements Listener {
         player.getInventory().clear();
         player.setGameMode(GameMode.SURVIVAL);
         player.setFoodLevel(20);
-        player.getWorld().setPVP(true);
-        player.setHealth(18.0);
-        for (Player p :world.getPlayers()){
-            if (isOni(p)){
-                player.sendMessage(ChatColor.WHITE + "現在の鬼は" + ChatColor.RED + p.getDisplayName() + ChatColor.WHITE  + "です");
-            }else{
-                player.sendMessage(p.getDisplayName());
-            }
-        }
+        player.getWorld().setPVP(false);
+        player.setHealth(20.0);
 
-        if (player.getWorld().getPlayers().size() == 1) {
-            Location location = new Location(player.getWorld(),1783,4,219);
-            player.teleport(location);
-            player.sendTitle(ChatColor.WHITE + "あなたは" + ChatColor.RED + "鬼" + ChatColor.WHITE + "です", "プレイヤーを捕まえましょう", 60, 80, 60);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 1000 * 20, 0));
-        } else {
-            Location location1 = new Location(player.getWorld(),1715,4,219);
-            player.teleport(location1);
-            player.sendTitle(ChatColor.WHITE + "あなたは" + ChatColor.RED + "逃げる人" + ChatColor.WHITE + "です", "鬼から逃げきりましょう", 60, 80, 60);
-        }
 
     }
 
@@ -188,51 +181,49 @@ public class OniWorld implements Listener {
             }
         }
     }
-    @EventHandler
+
+    /*@EventHandler
     public void onFoodLevelChangeEvent(FoodLevelChangeEvent event){
         String worldname = event.getEntity().getWorld().getName();
         if (worldname.equals(this.worldName)){
             event.setCancelled(true);
             return;
         }
-    }
+    }*/
+
     @EventHandler
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
-        if (event.getEntity() instanceof Player) {
+        if (event.getEntity().getWorld().getName().equals(this.worldName)) {
             World world = event.getEntity().getWorld();
             Player damager = (Player) event.getDamager();
-            if (event.getEntity().getWorld().getName().equals(this.worldName)) {
-                //ダメージを受けたエンティティがプレイヤーの場合
-                if (event.getEntity() instanceof Player) {
-                    Player player = (Player) event.getEntity();
-                    if (this.isOni(damager) ) {
-                        if (isEscaper(player)){
-                            player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 1000 * 20, 0));
-                            boolean allOni = true;
-                            for (Player p : event.getEntity().getWorld().getPlayers()) {
-                                if (isEscaper(p)){
-                                    allOni = false;
-                                }
-                            }
-                            for (Player p : event.getEntity().getWorld().getPlayers()) {
-                                p.sendTitle("鬼が増えました！", "「" + player.getName() + "」さんが鬼になりました", 40, 40, 40);
-                            }
-                            if (allOni){
-                                //終了
-                                for (Player p :world.getPlayers()){
-                                    p.sendMessage("ゲームが終了しました。ロビーに戻ります");
-                                    new OniCountDownScheduler(this.plugin, p,5).runTaskTimer(this.plugin,0,20);
-                                    p.removePotionEffect(PotionEffectType.GLOWING);
-                                }
-                            }
-                        }
-                        else{
+            //ダメージを受けたエンティティがプレイヤーの場合
+            if (event.getEntity() instanceof Player) {
+                Player player = (Player) event.getEntity();
+                if (event.getDamager().getUniqueId() == firstoni) {
+                    boolean allOni = true;
+                    for (Player p : event.getEntity().getWorld().getPlayers()) {
+                        if (isEscaper(p)) {
+                            allOni = false;
                         }
                     }
+                    for (Player p : event.getEntity().getWorld().getPlayers()) {
+                        p.sendTitle("鬼が増えました！", "「" + player.getName() + "」さんが鬼になりました", 40, 40, 40);
+                    }
+                    if (allOni) {
+                        //終了
+                        for (Player p : world.getPlayers()) {
+                            p.sendMessage("ゲームが終了しました。ロビーに戻ります");
+                            new OniCountDownScheduler(this.plugin, p, 5).runTaskTimer(this.plugin, 0, 20);
+                            p.removePotionEffect(PotionEffectType.GLOWING);
+                        }
+                    }
+                } else {
                 }
             }
         }
     }
+
+
     //鬼という定義を作る
     private boolean isOni(Player player) {
         if (player.hasPotionEffect(PotionEffectType.GLOWING)){
